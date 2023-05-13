a8e16d7847deac2d8912f4562e9710b9b95d02ba
Nazia Shehnaz Joynab <ncoder76@gmail.com>
2023-05-13 08:06:39 +0600

    Time: 0 ms (100.00%) | Memory: 6.4 MB (55.74%) - LeetSync

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    int f(int s, int e, vector<vector<int>>& dp)
-    {
-        if(s==0 and e==0)
-        {
-            return 1;
-        }
-        if(s<0 or e<0)
-        {
-            return 0;
-        }
-        if(dp[s][e]!=-1) return dp[s][e];
-        int up=f(s-1, e, dp);
-        int left=f(s, e-1, dp);
-        return dp[s][e]=up+left;
-    }
     int uniquePaths(int s, int e) {
         vector<vector<int>>dp(s, vector<int>(e, -1));
-        return f(s-1, e-1, dp); 
+        dp[0][0]=1;
+        for(int i=0;i<s;i++)
+        {
+            for(int j=0;j<e;j++)
+            {
+                if(i==0 and j==0)
+                {
+                    dp[0][0]=1;
+                }
+                else
+                {
+                    int up=0;
+                    int left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[s-1][e-1];
     }
 };
