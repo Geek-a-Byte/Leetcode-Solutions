c3e50bf7033cbf7139765242362616712213472e
Nazia Shehnaz Joynab <ncoder76@gmail.com>
2023-05-13 19:15:50 +0600

    Time: 552 ms (12.93%) | Memory: 13.1 MB (72.30%) - LeetSync

--- partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -19,19 +19,35 @@ public:
         if(t>=arr[i]) take=fn(i-1, t-arr[i], arr, dp);
         return dp[i][t]=((take) or (ntake));
     }
-    bool canPartition(vector<int>& nums) {
-         int sum=0;
-         for(auto i:nums)
-         {
-             sum+=i;
-         }
-         if(sum%2==1)
-         {
-             return false;
-         }
-         int k=sum/2;
-         int n=nums.size();
-         vector<vector<int>>dp(n, vector<int>(k+1,-1));
-         return fn(n-1, k, nums, dp);
+    bool canPartition(vector<int>& arr) {
+        int n=arr.size();
+        if(n==1) return false;
+        int sum=0;
+        for(auto i:arr)
+        {
+            sum+=i;
+        }
+        if(sum%2==1)
+        {
+            return false;
+        }
+        int k=sum/2;
+        vector<vector<bool>>dp(n, vector<bool>(k+1,0));
+        for(int i=0;i<n-1;i++)
+        {
+            dp[i][0]=true;
+        }
+        if(arr[0]<k) dp[0][arr[0]]=true;
+        for(int i=1;i<n;i++)
+        {
+            for(int t=1;t<=k;t++)
+            {
+                bool ntake=dp[i-1][t];
+                bool take=false;
+                if(t>=arr[i]) take=dp[i-1][t-arr[i]];
+                dp[i][t]=((take) or (ntake));
+            }
+        }
+        return dp[n-1][k];
     }
 };
