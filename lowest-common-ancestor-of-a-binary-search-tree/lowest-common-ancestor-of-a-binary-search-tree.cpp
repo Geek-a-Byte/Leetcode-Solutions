849ec63b841b2d05c572f5e3a476ed5f15d7bc69
Nazia Shehnaz Joynab <ncoder76@gmail.com>
2023-05-14 20:36:09 +0600

    Time: 27 ms (88.51%) | Memory: 23.3 MB (71.68%) - LeetSync

--- lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,66 +10,20 @@
 
 class Solution {
 public:
-    // Function should return all the ancestor of the target node
-    bool getPath(TreeNode* root, int x, vector<int>& arr)
-    {
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root==NULL)
         {
-            return false;
+            return NULL;
         }
-        
-        arr.push_back(root->val);
-        
-        if(root->val==x)
+        int curr=root->val;
+        if(curr<p->val and curr<q->val)
         {
-            return true;
+            return lowestCommonAncestor(root->right, p, q);
         }
-        
-        if(getPath(root->left, x, arr) or getPath(root->right, x, arr))
-        {
-            return true;
+        else if(curr>p->val and curr>q->val){
+            return lowestCommonAncestor(root->left, p, q);
         }
-        
-        arr.pop_back();
-        return false;
-    }
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<int>v;
-        int ans=getPath(root, p->val, v);
-        // v.pop_back();
-        // sort(v.begin(),v.end());
-
-        vector<int>v2;
-        ans=getPath(root, q->val, v2);
-        // v2.pop_back();
-        // sort(v2.begin(),v2.end());
-        
-        for(auto i:v)
-        {
-            cout<<i<<" ";
-        }
-        cout<<endl;
-        for(auto i:v2)
-        {
-            cout<<i<<" ";
-        }
-        
-        int n=v2.size();
-        int m=v.size();
-        int f=0;
-        TreeNode* ansNode=NULL;
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(v[i]==v2[j])
-                {
-                   ansNode=new TreeNode(v2[j]);
-                   break;
-                }
-            }
-        }
-        // TreeNode* ansNode=new TreeNode(v2[i-1]);
-        return ansNode;
+        return root;
     }
 };
