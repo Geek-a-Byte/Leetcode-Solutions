f1dbeb894b4cb629f7026d998a4bbcbb05f4aaad
Nazia Shehnaz Joynab <ncoder76@gmail.com>
2023-05-23 14:40:37 +0600

    Time: 28 ms (12.97%) | Memory: 6.1 MB (16.67%) - LeetSync

---
+++ palindrome-number/palindrome-number.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        string s=to_string(x);
+        string ss="";
+        for(auto i:s)
+        {
+            if((i>=65 and i<=90) or (i>=97 and i<=122) or (i>='0' and i<='9'))
+            {
+                char j=tolower(i);
+                ss+=j;
+            }
+        }
+        cout<<ss<<endl;
+        int n=ss.length();
+        int half=n/2;
+        int f=0;
+        for(int i=0;i<half;i++)
+        {
+            if(ss[i]!=ss[n-i-1])
+            {
+               f=1;
+               break;
+            }
+        }
+        if(f==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
